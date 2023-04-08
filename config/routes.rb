299de1ae24fec24de5996d1c4d33b15794f855fc
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-08 22:41:55 +0300

    Adding links to single blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "blog_posts#index"
 
-  get "/blog_posts/:id", to: "blog_posts#show"
+  resources :blog_posts
 end

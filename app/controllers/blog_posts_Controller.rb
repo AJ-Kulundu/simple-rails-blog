1af86b804e682ff4ef749a18968d2bff7776ff8c
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-08 23:05:07 +0300

    Enabling blog creation

--- app/controllers/blog_posts_Controller.rb
+++ app/controllers/blog_posts_Controller.rb
@@ -10,4 +10,22 @@ class BlogPostsController < ApplicationController
         redirect_to root_path
     end
 
+    def new
+        @blog_post = BlogPost.new
+    end
+
+    def create
+        @blog_post = BlogPost.new(blog_post_params)
+        if @blog_post.save
+            redirect_to @blog_post
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def blog_post_params
+        params.require(:blog_post).permit(:title, :body)
+    end
+
 end

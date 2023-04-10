2f33a02290e44ea41f724af49cb8ee8a6cd8f404
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-10 12:26:15 +0300

    Creating model scopes and helper functions

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,22 @@
 class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+     
+    scope :sorted, ->{order("publishedAt DESC, updated_at DESC")}
+    scope :draft, ->{where(publishedAt: nil)}
+    scope :published, ->{where("publishedAt <= ?",Time.current)}
+    scope :scheduled, ->{where("publishedAt > ?",Time.current)}
+
+    def draft?
+        publishedAt.nil?
+    end
+
+    def published?
+        publishedAt? && publishedAt <= Time.current
+    end
+
+    def scheduled?
+        publishedAt? && publishedAt > Time.current
+    end
+
 end

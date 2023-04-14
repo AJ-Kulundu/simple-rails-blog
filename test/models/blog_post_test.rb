325f8271a4e8bda3ba9406288a2a6735e378556d
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-14 23:49:55 +0300

    Creating a simple test

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    assert BlogPost.new(publishedAt: nil).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    refute BlogPost.new(publishedAt: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute BlogPost.new(publishedAt: 1.year.from_now).draft?
+  end
+
+
 end
